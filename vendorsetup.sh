7a0b61ee2abcbf75c7769dbb43995f4effdee261
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-09-11 23:03:39 +0530

    rmx3371: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,9 +8,7 @@ echo "cloning proprietary vendor"
 git clone https://github.com/Dragon-s-Playground/vendor_realme_rmx3371.git vendor/realme/rmx3371
 
 echo "cloning kernel"
-git clone https://github.com/Realme-SM8250/kernel_realme_sm8250.git -b test kernel/realme/sm8250
+git clone https://github.com/RealmeGTNeo3T-Devs/android_kernel_realme_spartan.git kernel/realme/sm8250
 
 echo "cloning oplus hals"
 git clone https://github.com/Dragon-s-Playground/android_hardware_oplus.git hardware/oplus
-
-git clone https://gitlab.com/ThankYouMario/proprietary_vendor_oplus_camera.git -b topaz-porsche vendor/oplus/camera
