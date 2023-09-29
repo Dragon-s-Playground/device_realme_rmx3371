96ee70f52007c4be2c82f215cc6d3de6f30adf84
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2023-09-29 08:52:24 +0530

    rmx3371: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,13 +5,18 @@ echo "cloning common vendor"
 git clone https://github.com/Dragon-s-Playground/vendor_realme_sm8250-common.git vendor/realme/sm8250-common
 
 echo "cloning proprietary vendor"
-git clone https://github.com/Dragon-s-Playground/vendor_realme_rmx3371.git -b opcam vendor/realme/rmx3371
+git clone https://github.com/Dragon-s-Playground/vendor_realme_rmx3371.git vendor/realme/rmx3371
 
 echo "cloning kernel"
 git clone https://github.com/RealmeGTNeo3T-Devs/android_kernel_realme_spartan.git kernel/realme/sm8250
 
+echo"Add kernelSU support"
+cd kernel/realme/sm8250
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+cd ../../..
+
 echo "cloning oplus hals"
-git clone https://github.com/Dragon-s-Playground/android_hardware_oplus.git -b opcam-arrow hardware/oplus
+git clone https://github.com/Dragon-s-Playground/android_hardware_oplus.git  hardware/oplus
 
 echo "cloning oplus camera"
 git clone https://gitlab.com/pjgowtham/proprietary_vendor_oplus_camera.git vendor/oplus/camera
